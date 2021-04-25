1e77d3d1073e154243eee1bafc91b0a420384b12
Dave <dave.barnett@jollygood.org.uk>
2021-04-25 20:37:00 +0100

    #1 Oranger.  Still not quite right #3 Flow averaged in both directions, but still asymmetrical.  Delay increased.

--- arduino_lava.ino
+++ arduino_lava.ino
@@ -9,12 +9,24 @@ int cur = 1;
 
 CRGB lava_color(int heat)
 {
-  if (heat < 92)
-    return CRGB((heat*255)/92, 0, 0);           // Black to Red
-  else if (heat < 184)
-    return CRGB(255, ((heat-92)*255)/92, 0);    // Red to Yellow
-  else
-    return CRGB(255, 255, ((heat-184)*255)/73); // Yellow to White
+  const int red_heat = 20;
+  const int orange_heat = 180;
+  const int yellow_heat = 230;
+  const int white_heat = 255;
+
+  const int red_red      = 150;
+  const int orange_green = 75;
+  const int yellow_green = 150;
+  const int white_blue   = 100;
+  
+  if (heat < red_heat)
+    return CRGB((heat * red_red) / red_heat, 0, 0);           // Black to Red
+  else if (heat < orange_heat)
+    return CRGB(red_red, ((heat - red_heat) * orange_green) / (orange_heat - red_heat), 0);    // Red to Orange
+  else if (heat < yellow_heat)
+    return CRGB(red_red, orange_green + ((heat - orange_heat) * (yellow_green - orange_green)) / (yellow_heat - orange_heat), 0);    // Orange to Yellow
+  else 
+    return CRGB(red_red, yellow_green, ((heat - yellow_heat) * white_blue) / (white_heat - yellow_heat)); // Yellow to White
 }
 
 void setup() {
@@ -37,7 +49,7 @@ void loop()
   int other = 1-cur;
   for(int i=0; i<NUM_LEDS; i++)
   {
-    H = (9 * heat[i][cur] + heat[(i+1)%NUM_LEDS][cur])/10; // Weighted average of surroundings
+    H = (1 * heat[(i+NUM_LEDS-1)%NUM_LEDS][cur] + 18 * heat[i][cur] + 4 * heat[(i+1)%NUM_LEDS][cur])/23; // Weighted average of surroundings
     heat[i][other] = max(H, heat[i][cur]); // Heat is weighted average or own heat, whichever is higher, with 1 cooling
     cool(heat[i][other]);
     leds[i] = lava_color((int)heat[i][other]);
@@ -52,6 +64,5 @@ void loop()
     heat[random(NUM_LEDS)][cur] = random(256);
   }
 
-
-  delay(1);
+  delay(5);
 }
